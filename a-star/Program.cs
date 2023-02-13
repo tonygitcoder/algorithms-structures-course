6776ce110ce41d906ed58e9b1e3f5cad7c1452a9
Tony <akruk@kse.org.ua>
2023-02-14 01:05:08 +0200

    Added GetNeighbours function

--- a-star/Program.cs
+++ a-star/Program.cs
@@ -30,8 +30,57 @@ internal class Program
             };
             var origins = new Dictionary<Point, Point>();
 
+            Console.WriteLine("GetNeighbours: " + GetNeighbours(5, 5, map).Count);
+
             // TODO: return path
             return null;
+
+
+            List<Point> GetNeighbours(int column, int row, string[,] map)
+            {
+                var neighbours = new List<Point>();
+
+                bool IsTraversable(Point point) => CheckPosition(point, map) == " ";
+
+
+                var topNeighbour = new Point(column, row - 1);
+                if (IsTraversable(topNeighbour))
+                {
+                    neighbours.Add(topNeighbour);
+                }
+
+                var bottomNeighbour = new Point(column, row + 1);
+                if (IsTraversable(bottomNeighbour))
+                {
+                    neighbours.Add(bottomNeighbour);
+                }
+
+                var leftNeighbour = new Point(column - 1, row);
+                if (IsTraversable(leftNeighbour))
+                {
+                    neighbours.Add(leftNeighbour);
+                }
+
+                var rightNeighbour = new Point(column + 1, row);
+                if (IsTraversable(rightNeighbour))
+                {
+                    neighbours.Add(rightNeighbour);
+                }
+
+                return neighbours;
+            }
+
+            string CheckPosition(Point point, string[,] map)
+            {
+                var leftBorder = point.Column < 0;
+                var rightBorder = point.Column >= map.GetLength(0);
+                var topBorder = point.Row < 0;
+                var bottomBorder = point.Row >= map.GetLength(1);
+
+                // TODO: catch exception
+                if (leftBorder || rightBorder || topBorder || bottomBorder) return "";
+                return map[point.Column, point.Row];
+            }
         }
     }
 }
