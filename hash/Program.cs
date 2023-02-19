ede756dd828e2b6f28c114af4dacfe302cc3d1ef
Tony <akruk@kse.org.ua>
2023-02-19 18:54:18 +0200

    Added Interface

--- hash/Program.cs
+++ hash/Program.cs
@@ -1,7 +1,37 @@
 ﻿using hash;
 
-var stringsDictionary = new StringsDictionary();
-stringsDictionary.Add("me", "18");
-Console.WriteLine(stringsDictionary.Get("me"));
-stringsDictionary.Remove("me");
-Console.WriteLine(stringsDictionary.Get("me"));
+var stringsDictionary = new StringsDictionary(100000);
+// stringsDictionary.Add("me", "18");
+// Console.WriteLine(stringsDictionary.Get("me"));
+// stringsDictionary.Remove("me");
+// Console.WriteLine(stringsDictionary.Get("me"));
+
+
+var lines = File.ReadAllLines("../../../dictionary.txt");
+for (var i = 0; i < lines.Length;)
+{
+    var line = lines[i];
+    var lineParts = line.Split(";", 2);
+    var key = lineParts[0];
+    var value = lineParts[1];
+    stringsDictionary.Add(key, value);
+
+    i++;
+    // Console.WriteLine($"Caching Progress: {i * 100 / lines.Length}% ({i}/{lines.Length})");
+}
+
+while (true)
+{
+    Console.WriteLine("> Input a word you would like to have definition for:");
+    var key = Console.ReadLine().ToUpper();
+
+    try
+    {
+        var value = stringsDictionary.Get(key);
+        Console.WriteLine($"> The definition is: {value}");
+    }
+    catch (KeyNotFoundException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
